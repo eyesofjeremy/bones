99389b3c14438ba1a50a2c485b39ac9723b6b700
Jeremy Carlson <info@jeremycarlson.com>
2016-09-20 13:30:43 -0600

    Integrate Quick Contact Info plugin

---
+++ library/plugins/quick-contact-info/quick-contact-info.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Plugin Name: Quick Contact Info
+Plugin URI: http://jeremycarlson.com/
+Description: Set up contact info to be accessible from anywhere in your site.
+Author: Jeremy Carlson
+Version: 0.2
+Author URI: http://www.jeremycarlson.com
+*/
+
+add_action( 'admin_menu', 'qcinfo_add_admin_menu' );
+add_action( 'admin_init', 'qcinfo_settings_init' );
+
+// Include addressifier, which allows us to make responsive-friendly abbreviations
+include('addressifier.php');
+
+// Include shortcodes
+include('site-option-shortcodes.php');
+
+function qcinfo_add_admin_menu(  ) { 
+
+	add_options_page( 'Contact Info', 'Contact Info', 'manage_options', 'contact-info', 'qcinfo_options_page' );
+
+}
+
+
+function qcinfo_settings_init(  ) { 
+
+	register_setting( 'clientSettings', 'qcinfo_settings' );
+
+	//
+	//
+	// Contact Info
+	//
+	add_settings_section(
+		'qcinfo_info_section', 
+		__( 'Contact Info', 'wordpress' ), 
+		'qcinfo_info_section_callback', 
+		'clientSettings'
+	);
+
+	// Phone Number
+	add_settings_field( 
+		'phone_number', 
+		__( 'Main Contact Number (needs to be U.S. number!)', 'wordpress' ), 
+		'qcinfo_phone_number_render', 
+		'clientSettings', 
+		'qcinfo_info_section' 
+	);
+
+	// Address
+	add_settings_field( 
+		'address', 
+		__( 'Main Address', 'wordpress' ), 
+		'qcinfo_address_render', 
+		'clientSettings', 
+		'qcinfo_info_section' 
+	);
+
+	// Email Address
+	add_settings_field( 
+		'email', 
+		__( 'Public Email Address', 'wordpress' ), 
+		'qcinfo_email_render', 
+		'clientSettings', 
+		'qcinfo_info_section' 
+	);
+
+/*
+	in case we want these....
+
+	add_settings_field( 
+		'checkbox_field_2', 
+		__( 'Settings field description', 'wordpress' ), 
+		'checkbox_field_2_render', 
+		'clientSettings', 
+		'info_section' 
+	);
+
+	add_settings_field( 
+		'radio_field_3', 
+		__( 'Settings field description', 'wordpress' ), 
+		'radio_field_3_render', 
+		'clientSettings', 
+		'info_section' 
+	);
+
+	add_settings_field( 
+		'select_field_4', 
+		__( 'Settings field description', 'wordpress' ), 
+		'select_field_4_render', 
+		'clientSettings', 
+		'info_section' 
+	);
+*/
+
+}
+
+
+function qcinfo_phone_number_render(  ) { 
+
+	$options = get_option( 'qcinfo_settings' );
+	?>
+	<input type='text' name='qcinfo_settings[phone_number]' value='<?php echo $options['phone_number']; ?>'>
+	<?php
+
+}
+
+
+function qcinfo_address_render(  ) { 
+
+	$options = get_option( 'qcinfo_settings' );
+	?>
+	<textarea cols='40' rows='5' name='qcinfo_settings[address]'><?php
+		echo $options['address']; 
+	?></textarea>
+	<?php
+
+}
+
+
+function qcinfo_email_render(  ) { 
+
+	$options = get_option( 'qcinfo_settings' );
+	?>
+	<textarea cols='40' rows='5' name='qcinfo_settings[email]'><?php
+		echo $options['email']; 
+	?></textarea>
+	<?php
+
+}
+
+
+function qcinfo_info_section_callback(  ) { 
+
+	echo __( 'Basic info about your organization', 'wordpress' );
+
+}
+
+
+function qcinfo_options_page(  ) { 
+
+	?>
+	<form action='options.php' method='post'>
+		
+		<h2>Contact Info and Site Details</h2>
+		
+		<?php
+		settings_fields( 'clientSettings' );
+		do_settings_sections( 'clientSettings' );
+		submit_button();
+		?>
+		
+	</form>
+	<?php
+
+}
---
+++ library/plugins/quick-contact-info/site-option-shortcodes.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * Site option shortcodes
+ * Tie it all together, make it more modular
+ * Note: Requires addressifier
+ */
+
+
+/*
+ * [phone_number]
+ */
+function qcinfo_phone_number( $atts ) {
+
+	$options = get_option( 'qcinfo_settings' );
+	return $options['phone_number'];
+}
+add_shortcode( 'phone_number', 'qcinfo_phone_number' );
+
+
+/*
+ * [phone]
+ * Right now only works for U.S. phone numbers
+ */
+function qcinfo_phone( $atts ) {
+
+	$options = get_option( 'qcinfo_settings' );
+	
+	$phone_number = $options['phone_number'];
+	
+	// remove all non-numeric characters from phone number
+	$phone_num_condensed = preg_replace('/[^\d]/', '', $phone_number);
+
+	$output = '<a class="tel" href="tel://+1' . $phone_num_condensed . '">' . $phone_number . '</a>';
+	return $output;
+}
+add_shortcode( 'phone', 'qcinfo_phone' );
+
+
+/*
+ * [address]
+ */
+function qcinfo_address( $atts ) {
+
+	$options = get_option( 'qcinfo_settings' );
+	return $options['address'];
+}
+add_shortcode( 'address', 'qcinfo_address' );
+
+
+/*
+ * [address_link]
+ * [address_link text="Ad hoc address text"]
+ * Right now only works for U.S. addresses
+ */
+function qcinfo_address_link( $atts ) {
+
+	$options = get_option( 'qcinfo_settings' );
+	
+	$address = $options['address'];
+	
+	// Use text attribute if we have it.
+  $a = shortcode_atts( array(
+    'text' => $address
+  ), $atts );
+  
+  // addressify the text, and apply content filters to it.
+  $text = apply_filters( 'the_content', addressify( $a['text'] ) );
+  
+	$output = '<a class="map" href="' . addressify_map_url( $address ) . '">' . $text . '</a>';
+	return $output;
+}
+add_shortcode( 'address_link', 'qcinfo_address_link' );
+
+/*
+ * [get_email]
+ */
+function qcinfo_get_email( $atts ) {
+
+	$options = get_option( 'qcinfo_settings' );
+	return $options['email'];
+}
+add_shortcode( 'get_email', 'qcinfo_email' );
+
+
+/*
+ * [email]
+ * [email text="Ad hoc address text"]
+ * Right now only works for U.S. addresses
+ */
+function qcinfo_email( $atts ) {
+
+	$options = get_option( 'qcinfo_settings' );
+	
+	$email = $options['email'];
+	
+	// Use text attribute if we have it.
+  $a = shortcode_atts( array(
+    'text' => $email
+  ), $atts );
+  
+	$output = '<a class="email" href="mailto:' . $email . '">' . $a['text'] . '</a>';
+	return $output;
+}
+add_shortcode( 'email', 'qcinfo_email' );
+
