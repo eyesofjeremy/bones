07dd1254cd04213e6a697f6bc9893db4bcba0ff0
Jeremy Carlson <info@jeremycarlson.com>
2016-09-20 13:27:45 -0600

    Remove Sidebar from layout and change main content area to full-width.
    Note: we are still leaving the inclusion of sidebar.php.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
-				<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
+<?php /* skip this.
+				<div id="sidebar1" class="sidebar m-all t-all d-all last-col cf" role="complementary">
 
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
@@ -7,9 +8,7 @@
 					<?php else : ?>
 
 						<?php
-							/*
-							 * This content shows up if there are no widgets defined in the backend.
-							*/
+							// This content shows up if there are no widgets defined in the backend.
 						?>
 
 						<div class="no-widgets">
@@ -19,3 +18,4 @@
 					<?php endif; ?>
 
 				</div>
+*/
