b96448113811274364eddb7d8ff05073d566ae9d
Jeremy Carlson <info@jeremycarlson.com>
2016-09-29 16:16:38 -0600

    Move promotions listing into archive template part
    This allows use on more places than the home page.

--- front-page.php
+++ front-page.php
@@ -36,40 +36,7 @@ get_header(); ?>
 								<a class="button" href="#">[ Schedule coming - discuss functionality ]</a>
 								</section>
 
-								<section class="promotions entry-content cf">
-								
-                <?php 
-      
-                  $page_id = get_the_ID();
-        
-                  // Look for promotions
-                  $promotions_listed = get_post_meta( $page_id, 'promotions_listed', true );
-
-                  if( ! empty( $promotions_listed ) ) {
-        
-                    // Set up the WP_Query object. Note: 'post__in' seems not to work unless we include post_type.
-                    // Perhaps this is b/c it is a custom post type.          
-
-                    $temp_query = $post; // first, store main query for retrieval
-
-                    $args = array(
-                      'post_type' => 'promotion',
-                      'post__in' => $promotions_listed,
-                      'orderby'   => 'post__in'
-                    );
-          
-                    $projects = new WP_Query( $args );
-                    
-                    while ( $projects->have_posts() ) : $projects->the_post();
-        
-                      get_template_part( 'template-parts/content', 'promotion' );
-          
-                    endwhile; // End of the loop.
-                    $post = $temp_query; // retrieve main query
-                  } // end if
-                ?>
-      
-								</section> <?php // end promotions ?>
+                <?php get_template_part('template-parts/archive', 'promotions'); ?>
 
 								<figure class="inspiration quote entry-content cf">
 								
