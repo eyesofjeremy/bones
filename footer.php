b5307d1a5886686999ea53a8fdf0a56c936b933c
Jeremy Carlson <info@jeremycarlson.com>
2017-06-20 16:16:56 -0600

    Add widget area to footer, and hide “No Widgets” message to average Joes
    Only users who can edit theme options will see the “no widgets” message if none are installed.
    Also: tweak to SCSS to remove background color from .no-widgets div.

--- footer.php
+++ footer.php
@@ -2,6 +2,27 @@
 
 				<div id="inner-footer" class="wrap cf">
 
+				<div id="sidebar-footer" class="sidebar m-all t-all d-all cf" role="complementary">
+
+					<?php if ( is_active_sidebar( 'sidebar-footer' ) ) : ?>
+
+						<?php dynamic_sidebar( 'sidebar-footer' ); ?>
+
+					<?php elseif ( current_user_can('edit_theme_options') ) : ?>
+
+						<?php
+							/*
+							 * This content shows up for admins if there are no widgets defined in the backend.
+							*/
+						?>
+
+						<div class="no-widgets">
+							<p><?php _e( 'This is a widget ready area. Add some and they will appear here.', 'bonestheme' );  ?></p>
+						</div>
+
+					<?php endif; ?>
+
+				</div>
 					<nav role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
