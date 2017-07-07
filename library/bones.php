2d64110645d1d8745034afeea80b4ca8b0c54e2d
Jeremy Carlson <info@jeremycarlson.com>
2017-07-07 16:48:30 -0600

    Polish navigation dropdown components
    This creates a click-based dropdown state. Works only if menu parent links are empty anchors (‘#’)
    
    Note: navigation.js is added to assist in ARIA states and adding .focus class

--- library/bones.php
+++ library/bones.php
@@ -144,6 +144,7 @@ function bones_scripts_and_styles() {
 
 		//adding scripts file in the footer
 		wp_register_script( 'bones-js', get_stylesheet_directory_uri() . '/library/js/scripts.js', array( 'jquery' ), '', true );
+		wp_register_script( 'bones-nav', get_stylesheet_directory_uri() . '/library/js/navigation.js', array( 'jquery' ), '', true );
 
 		// enqueue styles and scripts
 		wp_enqueue_script( 'bones-modernizr' );
@@ -160,6 +161,7 @@ function bones_scripts_and_styles() {
 		*/
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'bones-js' );
+		wp_enqueue_script( 'bones-nav' );
 
 	}
 }
---
+++ library/js/navigation.js
@@ -0,0 +1,70 @@
+/**
+ * navigation.js
+ *
+ * Handles toggling the navigation menu for small screens and enables tab
+ * support for dropdown menus.
+ *
+ * Modified from underscores theme
+ */
+( function() {
+	var container, menu, links, subMenus;
+
+	container = document.getElementById( 'site-navigation' );
+	if ( ! container ) {
+		return;
+	}
+
+	/**
+	 * Sets or removes .focus class on an element.
+	 */
+	function toggleFocus() {
+		var self = this;
+
+		// Move up through the ancestors of the current link until we hit .nav-menu.
+		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
+
+			// On li elements toggle the class .focus.
+			if ( 'li' === self.tagName.toLowerCase() ) {
+				if ( -1 !== self.className.indexOf( 'focus' ) ) {
+					self.className = self.className.replace( ' focus', '' );
+				} else {
+					self.className += ' focus';
+				}
+			}
+
+			self = self.parentElement;
+		}
+	}
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	menu.setAttribute( 'aria-expanded', 'false' );
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
+		menu.className += ' nav-menu';
+	}
+
+	// Get all the link elements within the menu.
+	links    = menu.getElementsByTagName( 'a' );
+	subMenus = menu.getElementsByTagName( 'ul' );
+
+	// Iterate through submenus
+	for ( var i = 0, len = subMenus.length; i < len; i++ ) {
+		var li = subMenus[i].parentNode,
+			liChildren = li.childNodes;
+			
+		// Set menu items with submenus to aria-haspopup="true".
+		li.setAttribute( 'aria-haspopup', 'true' );
+		
+		// set links before menus to not be active.
+		liChildren[0].setAttribute( 'href', '#' );
+	}
+
+	// Each time a menu link is focused or blurred, toggle focus.
+	for ( i = 0, len = links.length; i < len; i++ ) {
+	
+		links[i].setAttribute('tabindex', '0'); // fix for Safari / Opera
+		
+		links[i].addEventListener( 'focus', toggleFocus, true );
+		links[i].addEventListener( 'blur', toggleFocus, true );
+	}
+
+} )();
