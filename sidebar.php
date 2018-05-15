dd2e3a1416c46dab3aa0c911ec6a941c23e5c6bd
Jeremy Carlson <info@jeremycarlson.com>
2018-05-15 15:52:09 -0600

    Sidebar has bleed class but not utility ‘all’ classes

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-				<div id="sidebar1" class="sidebar m-all t-all d-all cf" role="complementary">
+				<div id="sidebar1" class="sidebar bleed cf" role="complementary">
 
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
